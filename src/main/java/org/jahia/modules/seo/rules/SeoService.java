a3fdfb54f8ea03a9a060e5fa955607f60100ad97
Sergiy Shyrkov <sshyrkov@jahia.com>
2015-12-22 22:31:43 +0100

    BACKLOG-6083 - File header changes

--- src/main/java/org/jahia/modules/seo/rules/SeoService.java
+++ src/main/java/org/jahia/modules/seo/rules/SeoService.java
@@ -15,10 +15,10 @@
  *
  *     IF YOU DECIDE TO CHOOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
  *
- *     This program is free software; you can redistribute it and/or
- *     modify it under the terms of the GNU General Public License
- *     as published by the Free Software Foundation; either version 2
- *     of the License, or (at your option) any later version.
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
  *     This program is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -26,8 +26,8 @@
  *     GNU General Public License for more details.
  *
  *     You should have received a copy of the GNU General Public License
- *     along with this program; if not, write to the Free Software
- *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *     along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
  *
  *     2/ JSEL - Commercial and Supported Versions of the program
  *     ===================================================================================
@@ -79,8 +79,8 @@ public class SeoService {
      */
     public void addMapping(final AddedNodeFact node, final String locale, final String url, final boolean isDefault,
                            KnowledgeHelper drools) throws RepositoryException {
-        if (logger.isDebugEnabled()) {
-            logger.debug("Adding URL mapping for node " + node.getPath() + " and locale '" + locale + "'");
+        if (logger.isInfoEnabled()) {
+            logger.info("\n\nAdding URL mapping for node " + node.getPath() + " and locale '" + locale + "'\n\n");
         }
         final String path = node.getPath();
         String urlToTry = url;
